61ceb1c2f4694a671e68d1af78376324572d2d10
Karim Makhloufi <maclouf@gmail.com>
2021-04-19 01:22:27 +0200

    Added page as dependency in useEffect array

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [number, setNumber] = useState(23);
+  const [page, setPage] = useState(1);
   const [users, setUsers] = useState([]);
   const fetchUsers = async () => {
     const result = await axios.get(
-      "https://randomuser.me/api/?page=3&results=10&seed=abc"
+      "https://randomuser.me/api/?page=" + page + "&results=10&seed=abc"
     );
     console.log(result.data.results);
     setUsers(result.data.results);
   };
   console.log("Render of App component");
-  useEffect(fetchUsers, []);
+  useEffect(fetchUsers, [page]);
 
   return (
     <div>
       {users.map((el) => (
         <p>{el.email}</p>
       ))}
+      <button
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Next Page
+      </button>
     </div>
   );
 };
