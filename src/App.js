25cb926b29fad61a1429eb7038a3aebbde7fbdb9
Karim Makhloufi <maclouf@gmail.com>
2021-04-19 01:19:58 +0200

    Displaying emails

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
   const [number, setNumber] = useState(23);
   const [users, setUsers] = useState([]);
-  const fetchUsers = async () =>
-    setUsers(
-      await axios.get("https://randomuser.me/api/?page=3&results=10&seed=abc")
+  const fetchUsers = async () => {
+    const result = await axios.get(
+      "https://randomuser.me/api/?page=3&results=10&seed=abc"
     );
+    console.log(result.data.results);
+    setUsers(result.data.results);
+  };
   console.log("Render of App component");
-  fetchUsers();
+  useEffect(fetchUsers, []);
 
   return (
     <div>
-      <p>Hello World</p>
-      <p>{number}</p>
-      <button
-        onClick={() => {
-          console.log("button was clicked");
-          setNumber(number + 1);
-        }}
-      >
-        Click Me
-      </button>
+      {users.map((el) => (
+        <p>{el.email}</p>
+      ))}
     </div>
   );
 };
