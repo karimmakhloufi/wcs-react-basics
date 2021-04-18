36d5b93afa73accdc76a2f00317bd29659a36bdd
Karim Makhloufi <maclouf@gmail.com>
2021-04-19 01:13:09 +0200

    Installed axios and fetched data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [number, setNumber] = useState(23);
+  const fetchUsers = async () =>
+    console.log(
+      await axios.get("https://randomuser.me/api/?page=3&results=10&seed=abc")
+    );
   console.log("Render of App component");
+  fetchUsers();
+
   return (
     <div>
       <p>Hello World</p>
