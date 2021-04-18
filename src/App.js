c5a3fad4667c27a50727a5dfa94c3f75898f9261
Karim Makhloufi <maclouf@gmail.com>
2021-04-19 01:04:13 +0200

    Changing variable but not reflected in state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 const App = () => {
+  console.log("Render of App component");
+  let number = 23;
   return (
     <div>
       <p>Hello World</p>
+      <p>{number}</p>
+      <button
+        onClick={() => {
+          console.log("button was clicked");
+          number = number + 1;
+          console.log("Number is now", number);
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 };
